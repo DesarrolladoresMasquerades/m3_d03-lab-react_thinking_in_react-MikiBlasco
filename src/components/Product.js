281154e72a76b14b856a9bfbdaf109ebd07ac4bf
Miki Blasco <blascom@7@gmail.com>
2022-03-05 21:18:59 +0100

    table tb

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React from 'react';
 
-function ProductRow({products}) {
-    return ( 
-        <div>
-        {products.map((el) => {
+function ProductRow({ products }) {
+  return (
+    <div>
+      {products.map((el) => {
         return (
-            
           <div className="productRow">
-            {el.stocked 
-            ? <p className="productElement">{el.name}</p>
-            : <p className="red">{el.name}</p>
-            }
+            {el.stocked ? (
+              <p className="productElement">{el.name}</p>
+            ) : (
+              <p className="red">{el.name}</p>
+            )}
             <p className="productElement">{el.price}</p>
           </div>
         );
       })}
-     </div>
-     );
+
+     <table>
+         <tr>
+             <th>Name</th>
+             <th>Price</th>
+         </tr>
+         
+         {products.map((el) => {
+        return (
+          <tr>
+            {el.stocked ? (
+              <td className="productElement">{el.name}</td>
+            ) : (
+              <td className="red">{el.name}</td>
+            )}
+            <td className="productElement">{el.price}</td>
+          </tr>
+        );
+      })}
+
+         
+     </table>
+
+    </div>
+  );
 }
 
 export default ProductRow;
